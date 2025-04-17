6a17cfb07e2a16f3045b75ad22cbb1ad93e45fe7
P4ST4S <antoine.rospars@epitech.eu>
2025-04-17 13:53:02 +0200

    feat: add tailwind-merge dependency and update CustomTextInput to support password type

--- src/components/CustomTextInput.tsx
+++ src/components/CustomTextInput.tsx
@@ -1,56 +1,41 @@
-import { StyleSheet, Text, TextInput, TextInputProps, Touchable, TouchableOpacity, View } from "react-native";
+import {
+  Text,
+  TextInput,
+  TextInputProps,
+  TouchableOpacity,
+} from "react-native";
 import Block from "./Block";
+import { View } from "react-native";
+import { twMerge } from "tailwind-merge"; // facultatif si tu veux merger des classes proprement
+import { useState } from "react";
 
 interface CustomTextInputProps extends TextInputProps {
-  type?: 'default' | 'password';
+  type?: "default" | "password";
+  className?: string;
 }
 
-const CustomTextInput: React.FC<CustomTextInputProps> = ({ type = 'default', ...props}) => {
-
+const CustomTextInput: React.FC<CustomTextInputProps> = ({
+  type = "default",
+  className,
+  ...props
+}) => {
   return (
-    <Block style={styles.inputContainer}>
+    <Block className="w-4/5 items-center mb-4">
       <TextInput
-          {...props}
-          placeholderTextColor="#8A9299"
-          style={StyleSheet.flatten([styles.input, props.style])}
-    />
-    {type === 'default' ||
-    <TouchableOpacity style={styles.forgotButton}>
-        <Text style={styles.forgotText}>OUBLIÉ ?</Text>
-    </TouchableOpacity>}
+        {...props}
+        placeholderTextColor="#8A9299"
+        className={twMerge(
+          "w-full h-12 bg-white/10 rounded-xl px-4 py-3 text-[#CFD8DC] border border-transparent text-base",
+          className
+        )}
+      />
+      {type === "password" && (
+        <TouchableOpacity className="absolute right-4 top-1/2 -translate-y-2">
+          <Text className="text-[#45B6FE] text-xs font-medium">OUBLIÉ ?</Text>
+        </TouchableOpacity>
+      )}
     </Block>
   );
-}
+};
 
 export default CustomTextInput;
-
-const styles = StyleSheet.create({
-    inputContainer: {
-      width: '80%',
-      alignItems: 'center',
-      marginBottom: 16,
-    },
-    input: {
-      width: '100%',
-      height: 50,
-      backgroundColor: 'rgba(255, 255, 255, 0.07)',
-      borderRadius: 12,
-      paddingHorizontal: 16,
-      paddingVertical: 14,
-      color: '#CFD8DC',
-      borderWidth: 1,
-      borderColor: 'transparent',
-      fontSize: 16,
-    },
-    forgotButton: {
-      position: 'absolute',
-      right: 16,
-      top: '50%',
-      transform: [{ translateY: -10 }],
-    },
-    forgotText: {
-      color: '#45B6FE',
-      fontSize: 12,
-      fontWeight: '500',
-    },
-  });
