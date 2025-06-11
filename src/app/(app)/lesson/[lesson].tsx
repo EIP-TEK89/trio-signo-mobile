e2188961338f70adafc7d1bee675606f892ce940
Yann Lebib <114907235+LebibYann@users.noreply.github.com>
2025-06-11 14:23:20 +0200

    rebasing dev from main

--- src/app/(app)/lesson/[lesson].tsx
+++ src/app/(app)/lesson/[lesson].tsx
@@ -1,10 +1,12 @@
 import LessonComplete from '@/components/Lessons/LessonComplete';
 import PlayLesson from '@/components/Lessons/PlayLesson';
+import AppView from '@/components/Ui/AppView';
 import { getLessonRequest } from '@/services/lessons';
 import { Lesson, LessonProgress, LessonWithExercises, Sign } from '@/types/LessonInterface';
 import { Link, router, useLocalSearchParams } from 'expo-router';
 import { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, Image } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function LessonScreen() {
     const { lesson } = useLocalSearchParams();
@@ -33,15 +35,17 @@ export default function LessonScreen() {
     if (loading){
         return (
           <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-            <Text style={{ color: '#fff' }}>Loading...</Text>
+            <Text style={{ color: '#fff' }}>Chargement...</Text>
           </View>
         );
       }
 
     return (
-      <View className='w-full h-full'>
+      <SafeAreaView className='flex-1 bg-background'> 
+      <AppView className='flex-1'>
         {lessonResult === null ? <PlayLesson lesson={lessonWithExercises} onComplete={lessonCompleted}/> : <LessonComplete lesson={lessonWithExercises} lessonProgress={lessonResult}/>}
-      </View>
+      </AppView>
+      </SafeAreaView>
     );
 }
 
---
+++ src/components/AIRecognizer/Recognize.tsx
@@ -0,0 +1,40 @@
+import { useEffect, useRef } from "react";
+import Block from "../Block"
+import { SignRecognizer } from "triosigno-lib";
+import { OnnxRunnerMobile } from "triosigno-mobile"
+import { Camera, CameraView, useCameraPermissions} from 'expo-camera'
+import { Button, Text, View } from "react-native";
+
+
+
+const Recognize: React.FC = () => {
+    const [permission, requestPermission] = useCameraPermissions();
+    const cameraRef = useRef<CameraView>(null);
+    
+    useEffect(() => {
+        
+    }, []);
+
+    if (!permission?.granted) {
+    // Camera permissions are not granted yet.
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} title="grant permission"/>
+      </View>
+    );
+  }
+
+    return (
+        <View className="flex-1 bg-white">
+            <CameraView
+                style={{ flex: 1 }}
+                facing={'front'}
+                ref={cameraRef}
+                >
+                </CameraView>
+        </View>
+    )
+}
+
+export default Recognize;
