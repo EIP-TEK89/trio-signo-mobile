785e00e00bb6a1bb98ead0f651898854b01fae0d
Linnchoeuh <lennintendo@gmail.com>
2025-07-04 18:10:19 +0200

    feat: add the ai recognition component

--- src/app/welcomeScreen.tsx
+++ src/app/welcomeScreen.tsx
@@ -1,6 +1,8 @@
 import AppView from "@/components/Ui/AppView";
 import Button from "@/components/Ui/Button";
 import Text from "@/components/Ui/Text";
+import Recognize from "@/components/AIRecognizer/Recognize";
+import React from "react";
 import { themeValues } from "@/constants/colorTheme";
 import { router } from "expo-router";
 import { SafeAreaView } from "react-native-safe-area-context";
@@ -10,6 +12,15 @@ export default function welcomeScreen() {
   return (
     <SafeAreaView className="flex-1 bg-background" edges={['top', 'bottom']}>
       <AppView className="flex-1 justify-center px-5" style={{ gap: 70 }}>
+        {/* <Recognize
+          model={"alphabet"}
+          sign_to_recognize={"a"}
+          onSuccess={setValue}
+          // webview_url="http://192.168.1.140:5173/ai-mobile-webview"
+          webview_url="https://triosigno.com/ai-recognition"
+          debug={true}
+        /> */}
+
         <AppView style={{ gap: 20 }}>
           <Text className="text-3xl font-black text-center ">Tu as déjà un compte ?</Text>
           <Button
--- src/components/AIRecognizer/Recognize.tsx
+++ src/components/AIRecognizer/Recognize.tsx
@@ -1,92 +1,36 @@
-import React, { useEffect, useState } from 'react';
-import { View, StyleSheet, Text, SafeAreaView } from 'react-native';
-import { SignRecognizer } from "triosignolib/core";
-import { OnnxRunnerMobile, MediapipeRunnerMobile } from "triosignolib/mobile"
-import { Camera, Frame, useFrameProcessor, useCameraDevice } from "react-native-vision-camera"
+import React from 'react';
+import { WebView } from 'react-native-webview';
+import { Dispatch, SetStateAction } from 'react';
 
-const Recognize: React.FC = () => {
-  const [hasPermission, setHasPermission] = useState(false);
-  const device = useCameraDevice('back')
-  let pickedDevice: number = 3; // front cam for my phone but maybe different for others
-  const onnx_runner: OnnxRunnerMobile = new OnnxRunnerMobile("models/alphabet.zip");
-  const mediapipe_runner: MediapipeRunnerMobile = new MediapipeRunnerMobile(
-    "$assets/models/hand_detector.tflite",
-    "$assets/models/hand_landmarker.tflite"
-  )
 
-  const sign_recon: SignRecognizer<Frame> = new SignRecognizer<Frame>(onnx_runner, mediapipe_runner)
+type RecognizeProps = {
+  model: string;
+  sign_to_recognize: string;
+  onSuccess: Dispatch<SetStateAction<boolean>>;
+  webview_url?: string;
+  debug?: boolean;
+};
 
-  useEffect(() => {
-    (async () => {
-      const status = await Camera.requestCameraPermission();
-      console.log(status)
-      const isGranted = status === 'granted';
-      setHasPermission(isGranted);
-      // if (isGranted) {
-      //   // console.log(devices);
 
-      //   for (let i = 0; i < devices.length; i++) {
-      //     console.log(`Device ${i}:`, devices[i]["name"], devices[i]["position"]);
-      //     console.log("Device capabilities:", devices[i]);
-      //     console.log("==========================")
-      //     if (devices[i]["position"] == "front") {
-      //       pickedDevice = i;
-      //       break;
-      //     }
-      //   }
-      // }
-      // console.log("Picked device:", pickedDevice);
-    })();
-  }, []);
-
-  const logFrameSize = (width: number, height: number) => {
-    // console.log(`Frame: ${width}x${height}`);
-  };
-
-  const logFrameSizeJS = Worklets.createRunOnJS(logFrameSize);
-
-  const frameProcessor = useFrameProcessor((frame) => {
-    'worklet';
-    logFrameSizeJS(frame.width, frame.height);
-  }, []);
-
-  if (pickedDevice == null) {
-    return (
-      <SafeAreaView style={styles.center}>
-        <Text>Loading camera devices...</Text>
-      </SafeAreaView>
-    );
-  }
-
-  if (!hasPermission) {
-    return (
-      <SafeAreaView style={styles.center}>
-        <Text>No camera permission</Text>
-      </SafeAreaView>
-    );
-  }
-
+const Recognize: React.FC<RecognizeProps> = ({
+  model,
+  sign_to_recognize,
+  onSuccess,
+  webview_url = "https://triosigno.com/ai-mobile-webview",
+  debug = false,
+}) => {
   return (
-    <View style={styles.container}>
-      <Camera
-        style={StyleSheet.absoluteFill}
-        device={device}
-        isActive={true}
-        frameProcessor={frameProcessor}
-      />
-    </View>
+    <WebView
+    mediaPlaybackRequiresUserAction={false}
+    onMessage={(event) => {
+      console.log("Message from webview:", event.nativeEvent.data);
+      onSuccess(true)
+    }}
+    scalesPageToFit
+      source={{ uri: `${webview_url}?model=${model}&label=${sign_to_recognize}&debug=${debug}` }}
+      style={{ flex: 1 }}
+    />
   );
 }
 
 export default Recognize
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-  },
-  center: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
